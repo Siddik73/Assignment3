a2fe9376203da6608715963a0751d16078ca73d4
Siddik73 <tanvir15-3873@diu.edu.bd>
2023-07-28 23:25:58 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    daisyui: {
+        themes: ["light", "dark", "cupcake"],
+    },
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+
+        },
+    },
+    plugins: [],
+
+}
